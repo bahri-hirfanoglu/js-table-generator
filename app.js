7d150baa44bdb87fdcb118e68008e5c840dae60a
bahrihrf34 <bahrihrf34@gmail.com>
2021-12-11 18:05:54 +0300

    package.json editing

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const app = express();
+
 app.use(express.static('public'))
-app.listen(4450)
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
+
+app.listen(process.env.PORT || 4450)
